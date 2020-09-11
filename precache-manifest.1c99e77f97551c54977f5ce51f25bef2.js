0dac71fb48ad4c5ca0462bea8ff6da1da535e4c2
Liudmyla Ilchenko <liudmyla.ilchenko7@gmail.com>
2020-09-11 18:26:35 +0300

    Updates

---
+++ precache-manifest.1c99e77f97551c54977f5ce51f25bef2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d876e4a5e511f39456bfff033a156d06",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ffbbe6e8b143fd2a13bc",
+    "url": "./static/css/main.f8e2330b.chunk.css"
+  },
+  {
+    "revision": "d1f9efed98ff8cda072a",
+    "url": "./static/js/2.d0afc09d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.d0afc09d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffbbe6e8b143fd2a13bc",
+    "url": "./static/js/main.e293a465.chunk.js"
+  },
+  {
+    "revision": "4d809184eca59fc21746",
+    "url": "./static/js/runtime-main.74611449.js"
+  }
+]);
